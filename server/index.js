057230b7e763603ebcb0623b93847c4dd9b7b170
Ahmad <ahmadpadarwala4@gmail.com>
2023-11-30 10:22:23 +0500

    server create new

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const PORT = require("./config/config");
+const dotenv = require("dotenv");
 const Brand_Route = require("./routes/Brand");
 const Category_Route = require("./routes/Category");
 const Product_Route = require("./routes/Product");
@@ -19,7 +20,7 @@ app.use(express.json());
 app.get("/", (req, res) => {
   return res.json("From Forolly Server Side");
 });
-
+dotenv.config();
 app.use("/", Brand_Route);
 app.use("/", Category_Route);
 app.use("/", Product_Route);
@@ -29,6 +30,6 @@ app.use("/", Nutrition_Route);
 app.use("/", Contact_Route);
 app.use("/", Inquiry_Route);
 
-app.listen(PORT, () => {
-  console.log(`Server Listening on port ${PORT}`);
+app.listen(process.env.PORT, () => {
+  console.log(`Server Listening on port` + process.env.PORT);
 });
